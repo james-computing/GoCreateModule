3583ed63ec0017a3eb82aa75d1bd2c2d4100d3e0
james <jamessimeao@outlook.com>
2026-02-23 13:34:57 -0300

    Handle empty string case as an error

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("John")
+	log.SetPrefix("greetings: ")
+	// Remove time stamp and source information
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 	fmt.Println(message)
 }
